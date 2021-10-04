3410e2f597cc6309a89c1ab2b1b599ab3b1b79bb
OnniL <onni.lukkarila@gmail.com>
2021-10-04 13:32:21 +0300

    Login.js added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link
+} from 'react-router-dom'
 
-function App() {
+import Login from './components/Login'
+import axios from 'axios'
+
+const App = () => {
+  const [events, setEvents] = useState([0])
+  const padding = {
+    padding: 5
+  }
+  useEffect(() => {
+    axios
+        .get('http://localhost:3001/events')
+        .then(response => {
+          console.log('promise fulfilled!!!')
+          setEvents(response.data)
+        })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="container">
+        {/*  */}
+        <Router>
+          <div>
+            <Link style={padding} to="/">login</Link>
+            <text>{localStorage.getItem("myUser")}</text>
+          </div>
+
+          <Switch>
+            <Route path="/">
+              <Login />
+            </Route>
+          </Switch>
+
+          <div>
+            <i>Esimerkkivalikko </i>
+          </div>
+        </Router>
+      </div>
+  )
 }
 
-export default App;
+export default App
